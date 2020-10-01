02fd4f6cfdacc3f39f49bcdf3cf0d518d5d4f623
GitHub Action <action@github.com>
2020-10-01 07:49:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/skrtks/clioncodamheader/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/skrtks/clioncodamheader/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.skrtks.clioncodamheader.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skrtks.clioncodamheader.MyBundle
 
 class MyApplicationService {
 
